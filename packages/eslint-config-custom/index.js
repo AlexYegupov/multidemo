e4bebf022ff9aa3c74e352d2b9ad6973e8368095
Alex Yegupov <yegupov@gmail.com>
2023-05-18 21:25:41 +0300

    turborepo+yarnberry monorepo + vite

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,20 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
+  env: {
+    node: true,
   },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
   parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "no-var": "error"
   },
 };
---
+++ packages/ui/components/counter.ts
@@ -0,0 +1,9 @@
+export function Counter() {
+  /*eslint no-var: "error"*/
+  //var a = 10;
+  //  a++;;
+  //console.log(`a:`, a)
+  //
+  console.log(`d`)
+  return `<button id="counter" type="button"></button>`;
+}

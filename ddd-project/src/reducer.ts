4e4eaa8b286593fe64d7f0258fb722b3bfda8031
Alex Yegupov <yegupov@gmail.com>
2023-06-21 10:10:23 +0300

    removed unused

--- ddd-project/src/reducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { reducers as exampleReducers, RootState as ExampleRootState } from "./domain/example/ducks";
-
-export type RootState = ExampleRootState;
-
-export default combineReducers({
-    ...exampleReducers,
-});
--- ddd-project/test.setup.js
+++
@@ -1,3 +0,0 @@
-import "@testing-library/jest-dom";
-
-globalThis.IS_REACT_ACT_ENVIRONMENT = true;
